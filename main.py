f8fb663de3232d64d91781bc92a1233dacd46977
Emuneshar <60401349+Emuneshar@users.noreply.github.com>
2023-05-03 21:11:10 +0000

    Added move function

--- main.py
+++ main.py
@@ -41,4 +41,21 @@ def goRight():
 
 def goLeft():
   head.direction = "left"
+
+def move():
+  if head.direction == "up":
+    y = head.ycor()
+    head.sety(y+20)
+
+  if head.direction == "down":
+    y = head.ycor()
+    head.sety(y-20)
+  
+  if head.direction == "right":
+    x = head.xcor()
+    head.setx(x+20)
+  
+  if head.direction == "left":
+    x = head.xcor()
+    head.sety(x-20)
   
